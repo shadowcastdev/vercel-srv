62db82e12fb49b7b60a88fd5e539e4b8607a28be
Sameer Singh <sameer@shadowcast.io>
2025-01-15 11:43:15 +0530

    logs

--- src/controller/main.controller.js
+++ src/controller/main.controller.js
@@ -1,13 +1,21 @@
 // Controller to handle POST request
 exports.handlePostRequest = (req, res) => {
   try {
-    const { data } = req.body; // Access the JSON payload
-    console.log("Received JSON:", data);
+    const { data } = req.body;
+    console.log("body payload:", data);
+
+    // Process stringify
+    const stringifyData = JSON.stringify(data);
+    console.log("stringifyData:", stringifyData);
+
+    // Convert JSON
+    const jsonData = JSON.parse(stringifyData);
+    console.log("jsonData:", jsonData);
 
     // Send response
     res.status(200).json({
       status: "success",
-      data: JSON.parse(data),
+      data: jsonData,
       message: "Received message successfully",
     });
   } catch (error) {
